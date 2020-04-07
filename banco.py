94ee0aac322d53c82d76ea3f4c1471072bc7fc02
Djalma Lima <61120198+djalmalima2@users.noreply.github.com>
2020-04-06 22:05:09 -0300

    Atualização_bancopy
    Eliminadas linhas desnecessárias; pequenos acertos; colocação de mensagens nos erros

--- banco.py
+++ banco.py
@@ -7,7 +7,6 @@ from typing import Union, List, Dict
 
 Number = Union[int, float]
 
-
 class Cliente():
     """
     Classe Cliente do Banco.
@@ -22,18 +21,14 @@ class Cliente():
 
     def __init__(self, nome: str, telefone: int, email: str):
         self.__nome = nome
-        self.__telefone = telefone
-        self.__email = email
-                     
         if type(telefone) is int:
             self.__telefone = telefone
         else:
-           raise TypeError
-            
+           raise TypeError("O telefone precisa ser um número inteiro!")
         if "@" in email:
             self.__email = email
         else:
-            raise ValueError
+            raise ValueError("O email deve conter o @!")
 
     def get_nome(self) -> str:
         """Acessor do atributo Nome."""
@@ -49,11 +44,10 @@ class Cliente():
         gera um TypeError
         """
         self.novo_telefone = novo_telefone
-        
         if type(novo_telefone) is int:
             self.__telefone = novo_telefone
         else:
-           raise TypeError
+           raise TypeError("O telefone precisa ser um número inteiro!")
 
     def get_email(self) -> str:
         """Acessor do atributo Email."""
@@ -65,11 +59,10 @@ class Cliente():
         (contendo o @), gera um ValueError.
         """
         self.novo_email = novo_email
-        
         if "@" in novo_email:
             self.__email = novo_email
         else:
-            raise ValueError
+            raise ValueError("O email deve conter o @!")
 
 
 class Banco():
@@ -87,7 +80,7 @@ class Banco():
    
     def __init__(self, nome: str):
         self.__nome = nome
-        self.contas = []
+        self.__contas = []
 
     def get_nome(self) -> str:
         """Acessor do Atributo Nome."""
@@ -102,20 +95,19 @@ class Banco():
         self.__clientes = clientes
         self.__saldo_ini = saldo_ini
         
-        if saldo_ini < 0:
-            raise ValueError
+        if saldo_ini <= 0:
+            raise ValueError("O saldo inicial deve ser maior que 0!")
         else:
             self.saldo_ini = saldo_ini
         
-        numero_de_contas = int(len(self.contas))
+        numero_de_contas = int(len(self.__contas))
         numero_conta = numero_de_contas + 1
         nova_conta = Conta(self.__clientes, numero_conta, self.__saldo_ini)
-        self.contas.append(nova_conta)
+        self.__contas.append(nova_conta)
 
     def lista_contas(self) -> List['Conta']:
         """Retorna a lista com todas as contas do banco."""
-        return list(self.contas)
-
+        return list(self.__contas)
 
 class Conta():
     """
@@ -133,15 +125,13 @@ class Conta():
     def __init__(self, clientes: List[Cliente], numero_conta: int, saldo_inicial: Number):
         self.__clientes = clientes
         self.__numero_conta = numero_conta
-        self.__saldo_inicial = saldo_inicial
         self.__saldo = 0
-        self.operações = []
-                        
-        if saldo_inicial < 0 :
-            raise ValueError
+        self.__operações = []
+        if saldo_inicial < 0:
+            raise ValueError("O saldo inicial deve ser maior que 0!")
         else:
             self.__saldo_inicial = saldo_inicial
-            self.operações.append(('saldo_inicial', saldo_inicial))
+            self.__operações.append(('saldo_inicial', saldo_inicial))
             self.__saldo = saldo_inicial
 
     def get_clientes(self) -> List[Cliente]:
@@ -170,12 +160,11 @@ class Conta():
         deve retornar um ValueError, e não efetuar o saque
         '''
         self.valor = valor
-        
         if valor > self.__saldo:
-            raise ValueError
+            raise ValueError("O valor do saque não pode ser maior que o saldo da conta!")
         else:
             self.__saldo = self.__saldo - valor
-            self.operações.append(('saque', valor))
+            self.__operações.append(('saque', valor))
 
     def deposito(self, valor: Number):
         '''
@@ -183,12 +172,12 @@ class Conta():
         '''
         self.valor = valor
         self.__saldo = self.__saldo + valor
-        self.operações.append(('deposito', valor))
+        self.__operações.append(('deposito', valor))
         
     def extrato(self) -> List[Dict[str, Number]]:
         '''
         Retorna uma lista com as operações (Tuplas) executadas na Conta
         '''
         extrato = []
-        extrato = self.operações
+        extrato = self.__operações
         return extrato
